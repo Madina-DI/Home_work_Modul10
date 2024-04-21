3257da82a9178ff127b4b0ae137bdf65edcfd7ff
Madina_DI <12mileik@rambler.ru>
2024-04-21 18:58:13 +0300

    second commit

--- Task3/main.js
+++ Task3/main.js
@@ -1,60 +1,61 @@
+const textError = document.querySelector('.error');
 const input = document.getElementById('message-input');
 const btn = document.getElementById('send-button');
+const location = document.getElementById('location_chat');
 const chatMessages = document.querySelector('.chat-messages');
-const textError = document.querySelector('error');
-const location = document.querySelector('.location_chat');
 
-const serverURL = "wss://echo-ws-service.herokuapp.com"
-
-webSocket = new WebSocket(serverURL);
-webSocket.onopen = function(evt) {
-    console.log("CONNECTED");
-};
-
-webSocket.onmessage = function(evt) {
-    writeToScreen(
-    '<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'
-    );
-};
-webSocket.onerror = function(evt) {
-    writeToScreen(
-    '<span style="color: red;">ERROR:</span> ' + evt.data
-    );
-};
+const serverURL = 'wss://echo-ws-service.herokuapp.com/';
+const webSocket = new WebSocket(serverURL);
 
 btn.addEventListener('click', () =>{
-    const massege = input.value;
+    const message = input.value;
 
     if(message ==""){
         textError.innerHTML = "Ошибка";
     } else {
-        webSocket.send(massege);
-        writeToScreen(`You: ${massege}`);
+        webSocket.send(message);
+        writeToScreen(`You: ${message}`, 'flex-end');
         input.value = "";
     }
 
-    });
+});
 
-function writeToScreen(massege, position = 'flex-end'){
-    let chatEl = `<p class = "chat-messages" style = "align-self: ${position}" ${massege}></p>`;
-    chatMessages.innerHTML += chatEl;
+webSocket.onopen = function(evt) {
+    console.log("CONNECTED");
+};
+webSocket.onmessage = function(evt) {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+};
+webSocket.onerror = function(evt) {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
 };
 
 
-const error = () =>{
+
+
+function writeToScreen(message, position = 'flex-end'){
+    const chatEl = document.createElement('p');
+    chatEl.classList.add('chat-messages');
+    chatEl.style.alignSelf = position;
+    chatMessages.innerHTML = message;
+    chatMessages.appendChild(chatEl);
+};
+
+
+const error = (err) =>{
     textError.innerHTML = "Allow access to your geolocation";
 };
 
 const success = (position) =>{
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    let link = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const link = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     writeToScreen(`<a href = '${link}' target = '_blanc'>Your geolocation</a>`);
 };
 
 
-location.addEventListener('click', () =>{
-    if(!navigator.geolocation){
+location.addEventListener('click', () => {
+    if(!navigator.geolocation) {
         textError.innerHTML = "This function is not supported by your browser";
      }
     else{
